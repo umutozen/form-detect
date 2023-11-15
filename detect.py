218c4da58c05cd10b884a63b649063778c091532
Umut Özen <writtenbyumut@gmail.com>
2023-11-15 13:56:11 +0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -22,7 +22,9 @@ def main():
     if not url.startswith(("http://", "https://")):
         url = "http://" + url
 
-    response = requests.get(url)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+
     soup = BeautifulSoup(response.text, 'html.parser')
     forms = soup.find_all('form')
 
